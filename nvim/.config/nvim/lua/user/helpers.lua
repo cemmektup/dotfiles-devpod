e7f3135a12c0cc354a8069b7e9c189645e0dda77
Cem Karakaya <cemmektup@gmail.com>
2024-08-09 22:41:29 +0200

    add srv project

--- nvim/.config/nvim/lua/user/helpers.lua
+++ nvim/.config/nvim/lua/user/helpers.lua
@@ -5,7 +5,9 @@ function M.string_starts(String, Start)
 end
 
 function M.directory_exists(sPath)
-  if type(sPath) ~= "string" then return false end
+  if type(sPath) ~= "string" then
+    return false
+  end
 
   local response = os.execute("cd " .. sPath)
   if response == 0 then
@@ -17,13 +19,15 @@ end
 -- check if the file exists and is readable
 function M.file_exists(path)
   local file = io.open(path, "rb")
-  if file then file:close() end
+  if file then
+    file:close()
+  end
   return file ~= nil
 end
 
 function M.get_visual_selection()
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
+  local s_start = vim.fn.getpos "'<"
+  local s_end = vim.fn.getpos "'>"
   local n_lines = math.abs(s_end[2] - s_start[2]) + 1
   local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
   lines[1] = string.sub(lines[1], s_start[3], -1)
@@ -32,7 +36,7 @@ function M.get_visual_selection()
   else
     lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
   end
-  return table.concat(lines, '\n')
+  return table.concat(lines, "\n")
 end
 
 function M.get_project_name()
@@ -41,6 +45,7 @@ end
 
 function M.get_application_name()
   local projects = {}
+  projects["srv"] = "app"
   projects["tina"] = "app"
   projects["ovam"] = "app"
   projects["lva"] = "bundle"

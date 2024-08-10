827990f716c39777e9c90fa79e671ca4fd1bcfc7
Cem Karakaya <cemmektup@gmail.com>
2024-08-10 03:18:14 +0200

    add php cs fixer as php formatter

--- nvim/.config/nvim/lua/custom/plugins/formatting.lua
+++ nvim/.config/nvim/lua/custom/plugins/formatting.lua
@@ -3,9 +3,9 @@ return {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local conform = require("conform")
+      local conform = require "conform"
 
-      conform.setup({
+      conform.setup {
         formatters_by_ft = {
           javascript = { "prettier" },
           typescript = { "prettier" },
@@ -22,20 +22,22 @@ return {
           twig = { "prettier" },
           lua = { "stylua" },
           python = { "isort", "black" },
+          php = { "php_cs_fixer" },
         },
         format_on_save = {
           lsp_fallback = true,
           async = false,
           timeout_ms = 1000,
         },
-      })
+      }
 
       vim.keymap.set({ "n", "v" }, "<leader>rf", function()
-        conform.format({
+        conform.format {
           lsp_fallback = true,
           async = false,
           timeout_ms = 1000,
-        })
+        }
       end, { desc = "format file or range (in visual mode)" })
     end,
-  } }
+  },
+}

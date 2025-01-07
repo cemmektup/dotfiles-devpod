961ef9a2c885f3b6084ccfb24028d7f5455b7b42
Cem Karakaya <cemmektup@gmail.com>
2025-01-07 17:40:44 +0100

    main: Update key mappings for network operation
    - Changed network port in key mappings from 8377 to 12345 to match the new network configuration.

--- nvim/.config/nvim/lua/user/key_mappings.lua
+++ nvim/.config/nvim/lua/user/key_mappings.lua
@@ -42,8 +42,8 @@ set("n", "N", "Nzzzv")
 -- paste without change the register
 set("x", "<leader><leader>p", [["_dP]], { noremap = true, desc = "Paste without change the register" })
 set({ "n", "v" }, "<leader><leader>d", [["_d]], { noremap = true, desc = "Delete without change the register" })
-vim.api.nvim_set_keymap("n", "<leader>yy", ":call system('nc localhost 8377', @0)<CR>", { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>yy", ":call system('nc localhost 8377', @0)<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>yy", ":call system('nc localhost 12345', @0)<CR>", { noremap = true })
+vim.api.nvim_set_keymap("v", "<leader>yy", ":call system('nc localhost 12345', @0)<CR>", { noremap = true })
 
 -- Move line(s) up and down
 vim.api.nvim_set_keymap("i", "<M-j>", "<Esc>:m .+1<CR>==gi", { noremap = true })
